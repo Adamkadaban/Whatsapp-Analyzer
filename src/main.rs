8e6970a566c3c96ea7806893670abe5abb7fb160
Adamkadaban <adamkadaban@gmail.com>
2024-03-16 18:49:48 -0400

    rewrote it to only use CsvReader. unfortunately, idk how to split into columns. It also seems that there is a bug (or maybe im just bad at coding). issue here: https://github.com/pola-rs/polars/issues/15101

--- src/main.rs
+++ src/main.rs
@@ -25,52 +25,77 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
+/*
 fn parse_whatsapp_file(filename: &String) {
 
-    let columns = BTreeMap::new();
-    
+    // let mut df = DataFrame::new();
+    // df.add_column("Date".to_string(), Vec::<NaiveDate>::new());
+    // df.add_column("Name".to_string(), Vec::<String>::new());
+    // df.add_column("Message".to_string(), Vec::<String>::new());
+
+    let mut dates = Vec::new();
+    let mut names = Vec::new();
+    let mut messages = Vec::new();
+
+   
     // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
     if let Ok(lines) = read_lines(filename) {
-        /*
-        Capture groups:
-            1 - date
-            2 - user
-            3 - message
-         */
-
         // note that the thing after the seconds and before AM/PM is not a space. Why? idk. whatsapp
-        // let re = Regex::new(r"\[(\d+/\d+/\d+, \d+:\d+:\d+ [AP]M)\] (.*): .*").unwrap();
-        let re = Regex::new(r"\[(.*)\] (.*): .*").unwrap();
+        let re = Regex::new(r"\[(.*)\] (.*): (.*)").unwrap();
         let mut i = 1;
 
         for line in lines.flatten() {
-            let caps = re.captures(&line).unwrap();
+            if let Some(caps) = re.captures(&line){
+                // https://dtantsur.github.io/rust-openstack/chrono/format/strftime/index.html
+                let date = NaiveDateTime::parse_from_str(
+                    &caps[1],
+                    "%D, %r" // weird whitespace between the two values. 
+                ).unwrap_or_else(|err| {
+                    eprintln!("Problem parsing arguments: {err}");
+                    process::exit(1);
+                });
 
-            // https://dtantsur.github.io/rust-openstack/chrono/format/strftime/index.html
-            let time = NaiveDateTime::parse_from_str(
-                &caps[1].replace(" ", " "),
-                "%D, %r" 
-            ).unwrap_or_else(|err| {
-                eprintln!("Problem parsing arguments: {err}");
-                process::exit(1);
-            });
-
-            columns.entry(time).or_insert(vec![]).push(&caps[2]);
-            columns.entry(time).or_insert(vec![]).push(&caps[3]);
-
+                dates.push(&date);
+                names.push(caps[2]);
+                messages.push(caps[3]);
+            } else {
+                // this line is not a new message. treat it as part of the previous message
+                continue;
+            }
             i += 1;
             if i > 100 {
                 break;
             }
             // println!("{}", time.format("around %l %p on %b %-d").to_string());
+
         }
     }
-    let df = DataFrame::new(
-        columns.into_iter()
-            .map(|(name, message)| Series::new(name, message))
-            .collect::<Vec<_>>()
+
+    let mut df = df!(
+        // "Date" => dates.iter().copied().collect::<Vec<_>>(),
+        "Name" => &names,
+        "Message" => &messages,
     ).unwrap();
 
+
+    println!("{}", df);
+}
+
+*/
+
+fn parse_whatsapp_file(filepath: &String) {
+    // let captures = col("column_1");
+    let mut df = CsvReader::from_path(filepath).expect("Filepath not found")
+        .with_separator('\r' as u8)
+        .has_header(false)
+        .with_columns(  // takes in Option<Vec<String>>
+            // captures.extract_groups(r"\[(.*)\] (.*): (.*)")
+            // Some(vec!["Date".to_string(), "Name".to_string(), "Message".to_string()])
+            Some(vec!["column_1".into()])
+        )
+        .truncate_ragged_lines(true)
+        .finish().unwrap();
+    
     println!("{}", df);
 }
 
@@ -80,6 +105,4 @@ fn main(){
     let filename = &args[1]; // filename
 
     parse_whatsapp_file(filename);
-
-
 }
