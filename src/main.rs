eda4bc84dc633e77f8dd69890e59c3ec0d2d0cbe
Adamkadaban <adamkadaban@gmail.com>
2024-03-15 23:02:14 -0400

    add file input and argv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use polars::prelude::*; // for data analysis
+use std::env; // for argv
+use std::fs; // for reading in file 
+
+
+fn parse_whatsapp_file(filename: &String) -> Option<> {
+    let contents = fs::read_to_string(filename)
+        .expect("Unable to read data from provided filepath");
+}
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+
+    let filename = &args[1]; // filename
+
+
 }
